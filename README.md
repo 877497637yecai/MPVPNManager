ccfbc9d0febff3c9dfcecec78895c100aa3780a5
mopellet <351420450@qq.com>
2016-07-11 15:18:59 +0800

    封装成MoVPNManage

--- README.md
+++ README.md
@@ -2,26 +2,55 @@
 IOS VPN代理 
 
 ## 联系方式 
-* Email:351420450@qq.com
-* WeChat:wzw351420450 
+* Email:mopellet@foxmail.com
 * 欢迎大家与我一起学习进步
 
 ## 项目目录
 * [Appointment](https://github.com/MoPellet/Appointment)
 * [MOMenuView](https://github.com/MoPellet/MOMenuView)
 
-
 一、项目介绍
 ----------
-1、导入NetworkExtension框架
+1、添加相关框架
+SystemConfiguration.framework
+Security.framework
+NetworkExtension.framework
+
+2、导入MoVPNManage.h 头文件到你的项目中
 ```c
-#import <NetworkExtension/NetworkExtension.h>
+#import "MoVPNManage.h"
 ```
-2、导入Reachability 检测网络状态
+
+3、创建MoVPNManage的实例
 ```c
-#import "Reachability.h"
+MoVPNManage * vpnManage = [MoVPNManage shareVPNManage];
 ```
-3、在不越狱的情况下支持 ipsec协议 
+
+4、设置标题
+```c
+[vpnManage setVpnTitle:@"Happy Vpn"];
+```
+
+5、配置相关参数 服务器 账号 密码 私钥
+```c
+[vpnManage setServer:@"your host" ID:@"id" pwd:@"pwd" privateKey:@"key"];
+```
+
+6、保存配置参数
+```c
+[vpnManage saveConfigCompleteHandle:^(BOOL success, NSString *returnInfo) {
+    NSLog(@"%@",returnInfo);
+    if (success) {
+        [vpnManage vpnStart];
+    }
+}];
+```
+7、启动VPN
+```c
+[vpnManage vpnStart];
+```
+
+8、在不越狱的情况下支持 ipsec协议 
 
 二、相关资料
 * [谈谈 iOS8 中的 Network Extension](http://blog.zorro.im/posts/iOS8-Network-Extension.html)
--- VpnTest/VpnTest/ViewController.m
+++ VpnTest/VpnTest/ViewController.m
@@ -7,173 +7,31 @@
 //
 
 #import "ViewController.h"
-#import <NetworkExtension/NetworkExtension.h>
-#import "AFNetworkReachabilityManager.h"
+#import "MoVPNManage.h"
 
-
-
-
-
-#pragma mark - 定义一些所需参数 可以替换成自己的 可以上淘宝买一个记得 只支持ipsec
-#define server @"108.61.180.50"
-#define ID @"chenziqiang01"
-#define pwd @"18607114709"
-#define privateKey @"tksw123"
-
-@interface ViewController ()
-@property(strong,nonatomic)NEVPNManager *vpnManager;
-@end
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setUp];
-}
-
-- (void)setUp{
-    // 将账号密码存入钥匙串
-    {
-        [self createKeychainValue:pwd forIdentifier:@"vpnPassWord"];
-        [self createKeychainValue:privateKey forIdentifier:@"sharedKey"];
-    }
     
-    //  这里才是核心代码 仅支持 ipsec 
-    // 1. 获取NEVPNManager实例
-    // 2. loadFromPreferencesWithCompletionHandler 加载设置
-    // 3. saveToPreferencesWithCompletionHandler 设置并存储设置
-    {
-        self.vpnManager = [NEVPNManager sharedManager];
-        [_vpnManager loadFromPreferencesWithCompletionHandler:^(NSError *error) {
-            if (error) {
-                NSLog(@"Load config failed [%@]", error.localizedDescription);
-                return;
-            }
-            NEVPNProtocolIPSec *p = (NEVPNProtocolIPSec*)_vpnManager.protocol;
-            if (!p) {
-                p = [[NEVPNProtocolIPSec alloc] init];
-            }
-            
-            p.username = ID;
-            p.serverAddress = server;
-            p.passwordReference = [self searchKeychainCopyMatching:@"vpnPassWord"];
-            p.authenticationMethod = NEVPNIKEAuthenticationMethodSharedSecret;
-            p.sharedSecretReference = [self searchKeychainCopyMatching:@"sharedKey"];
-            p.useExtendedAuthentication = YES;
-            p.disconnectOnSleep = NO;
-            _vpnManager.protocol = p;
-            _vpnManager.onDemandEnabled=YES;
-            _vpnManager.localizedDescription = @"IPSec Test"; //设置VPN的名字 可以自定义
-            
-            // 一定要保存
-            [_vpnManager saveToPreferencesWithCompletionHandler:^(NSError *error) {
-                if(error) {
-                    NSLog(@"Save error: %@", error);
-                }
-                else {
-                    NSLog(@"Saved!");
-                }
-            }];
-        }];
-    }
-    
-    
-    //检测网络 自动重连 （可选）
-    [self performSelector:@selector(registerNetWorkReachability) withObject:nil afterDelay:0.35f];
-}
-
-
-#pragma mark -
-#pragma mark - 操作方法 BEGIN
-- (IBAction)buttonPressed:(id)sender {
-    [self vpnStart];
-}
-
-- (IBAction)buttonStop:(id)sender {
-    [self vpnStop];
-}
-
--(void)vpnStart{
-    NSError *startError;
-    [_vpnManager.connection startVPNTunnelAndReturnError:&startError];
-    if (startError) {
-        NSLog(@"Start VPN failed: [%@]", startError.localizedDescription);
-    }
-}
-
--(void)vpnStop{
-    [_vpnManager.connection stopVPNTunnel];
-}
-#pragma mark - 操作方法 END
-
-#pragma mark -
-#pragma mark - 自动重连 BEGIN
-- (void)registerNetWorkReachability{
-    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(checkNetWork) name:AFNetworkingReachabilityDidChangeNotification object:nil];
-    
-}
-/**
- *  检测网络
- */
--(void)checkNetWork{
-    [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
-        if (status == AFNetworkReachabilityStatusReachableViaWWAN ||
-            status == AFNetworkReachabilityStatusReachableViaWiFi) {
-            if (self.vpnManager.connection.status != NEVPNStatusConnected) {
-                [self vpnStart];
-            }
+    MoVPNManage * vpnManage = [MoVPNManage shareVPNManage];
+    [vpnManage setVpnTitle:@"Happy Vpn"];
+    [vpnManage setServer:@"108.61.180.50" ID:@"chenziqiang01" pwd:@"18607114709" privateKey:@"tksw123"];
+    [vpnManage setReconnect:YES];
+    [vpnManage saveConfigCompleteHandle:^(BOOL success, NSString *returnInfo) {
+        NSLog(@"%@",returnInfo);
+        if (success) {
+            [vpnManage vpnStart];
         }
     }];
 }
-#pragma mark - 自动重连 END
 
-
-
-#pragma mark -
-#pragma mark - KeyChain BEGIN
-
-static NSString * const serviceName = @"im.zorro.ipsec_demo.vpn_config"; // 可以自定义
-
-- (NSMutableDictionary *)newSearchDictionary:(NSString *)identifier {
-    NSMutableDictionary *searchDictionary = [[NSMutableDictionary alloc] init];
-    
-    [searchDictionary setObject:(__bridge id)kSecClassGenericPassword forKey:(__bridge id)kSecClass];
-    
-    NSData *encodedIdentifier = [identifier dataUsingEncoding:NSUTF8StringEncoding];
-    [searchDictionary setObject:encodedIdentifier forKey:(__bridge id)kSecAttrGeneric];
-    [searchDictionary setObject:encodedIdentifier forKey:(__bridge id)kSecAttrAccount];
-    [searchDictionary setObject:serviceName forKey:(__bridge id)kSecAttrService];
-    
-    return searchDictionary;
+#pragma mark - 操作方法 BEGIN
+- (IBAction)buttonPressed:(id)sender {
+    [[MoVPNManage shareVPNManage] vpnStart];
 }
-
-- (NSData *)searchKeychainCopyMatching:(NSString *)identifier {
-    NSMutableDictionary *searchDictionary = [self newSearchDictionary:identifier];
-    
-    [searchDictionary setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
-    [searchDictionary setObject:@YES forKey:(__bridge id)kSecReturnPersistentRef];
-    
-    CFTypeRef result = NULL;
-    SecItemCopyMatching((__bridge CFDictionaryRef)searchDictionary, &result);
-    
-    return (__bridge_transfer NSData *)result;
+- (IBAction)buttonStop:(id)sender {
+    [[MoVPNManage shareVPNManage] vpnStop];
 }
-
-- (BOOL)createKeychainValue:(NSString *)password forIdentifier:(NSString *)identifier {
-    NSMutableDictionary *dictionary = [self newSearchDictionary:identifier];
-    
-    OSStatus status = SecItemDelete((__bridge CFDictionaryRef)dictionary);
-    
-    NSData *passwordData = [password dataUsingEncoding:NSUTF8StringEncoding];
-    [dictionary setObject:passwordData forKey:(__bridge id)kSecValueData];
-    
-    status = SecItemAdd((__bridge CFDictionaryRef)dictionary, NULL);
-    
-    if (status == errSecSuccess) {
-        return YES;
-    }
-    return NO;
-}
-#pragma mark - KeyChain END
 
 @end
